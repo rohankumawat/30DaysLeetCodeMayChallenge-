4a162bd7198465c864173956995f0b4b3e9053dd
Rohan Kumawat <51725975+rohankumawat@users.noreply.github.com>
2020-05-30 03:37:11 +0530

    Course Schedule solution

---
+++ Week5/Day29/CourseSchedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        indegree, outdegree = collections.defaultdict(set), collections.defaultdict(set)
+        for x, y in prerequisites:
+            outdegree[y].add(x)
+            indegree[x].add(y)
+            
+        connection_removed = 0
+        indegree_zero = []
+        for i in range(numCourses):
+            if not indegree[i]:
+                indegree_zero.append(i)
+                connection_removed += 1
+        
+        while indegree_zero:
+            node = indegree_zero.pop()
+            for x in outdegree[node]:
+                indegree[x].remove(node)
+                if not indegree[x]:
+                    indegree_zero.append(x)
+                    connection_removed += 1
+        
+        return connection_removed == numCourses
